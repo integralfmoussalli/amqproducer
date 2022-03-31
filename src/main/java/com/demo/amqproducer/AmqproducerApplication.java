cf32856c62454976239270a42d99ce783b4a09ec
fmoussalli <fmoussalli@integral.com.au>
2022-03-31 14:55:37 +1000

    Initial commit

---
+++ src/main/java/com/demo/amqproducer/AmqproducerApplication.java
@@ -0,0 +1,13 @@
+package com.demo.amqproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqproducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/amqproducer/AmqproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.amqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
